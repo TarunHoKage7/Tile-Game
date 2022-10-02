8c324ca14bc7d0e999d7eedf31660d5fdc8160fa
UserTarun <saikalyantarun2001@gmail.com>
2022-10-02 14:25:59 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*high = [15,25,35]; // localStorage
+translate value = 20px
+count = 0  1 2 3 4 5 6
+translate value = 20px + 2 * (count)
+
+count = 0
+flag = 0
+while(flag <= 5){
+
+
+speed = count//10
+card.style.transformY = 20 + speed * 10;
+
+if(white.isClicked() or !black.isclicked()){
+    flag += 1;
+}
+count += 1
+}
+
+if(count > max(high)){
+    high.push(count)
+    remove min of high
+    new screen, show(new high score) show(podium);
+}
+*/
